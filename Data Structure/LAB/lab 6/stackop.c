ba4c8666f1eef3cb1df08619829532e6f89be3bd
aadhyavaghela142-droid <aadhyavaghela142@gmail.com>
2026-01-07 20:08:08 +0530

    Update stackop.c

--- Data Structure/LAB/lab 6/stackop.c
+++ Data Structure/LAB/lab 6/stackop.c
@@ -1,102 +1,55 @@
-#include <stdio.h>
-#define MAX 5   
+#include<stdio.h>
+int s[100];
+int top=-1;
 
-int stack[MAX];
-int top = -1;
-
-void push() {
-    int value;
-    if (top == MAX - 1) {
-        printf("Stack Overflow! Cannot push.\n");
-    } else {
-        printf("Enter value to push: ");
-        scanf("%d", &value);
-        top++;
-        stack[top] = value;
-        printf("Value pushed successfully.\n");
+void push(int x){
+    if(top>=99){
+        printf("stack overflow");
     }
+    top++;
+    s[top]=x;
 }
-
-void pop() {
-    if (top == -1) {
-        printf("Stack Underflow! Cannot pop.\n");
-    } else {
-        printf("Popped value: %d\n", stack[top]);
-        top--;
+int pop(){
+    if(top<0){
+        printf("stack underflow");
+        return -1;
     }
+    top--;
 }
-
-void peep() {
-    int pos;
-    if (top == -1) {
-        printf("Stack is empty.\n");
-    } else {
-        printf("Enter position from top: ");
-        scanf("%d", &pos);
-
-        if (top - pos + 1 < 0) {
-            printf("Invalid position.\n");
-        } else {
-            printf("Element at position %d is %d\n", pos, stack[top - pos + 1]);
-        }
+int peep(int i){
+    if(top-i+1<=0){
+        printf("stack underflow");
+        return -1;
     }
+    return s[top-i+1];
 }
-
-void change() {
-    int pos, value;
-    if (top == -1) {
-        printf("Stack is empty.\n");
-    } else {
-        printf("Enter position from top: ");
-        scanf("%d", &pos);
-
-        if (top - pos + 1 < 0) {
-            printf("Invalid position.\n");
-        } else {
-            printf("Enter new value: ");
-            scanf("%d", &value);
-            stack[top - pos + 1] = value;
-            printf("Value changed successfully.\n");
-        }
+void change(int i,int x){
+    if(top-i+1<=0){
+        printf("stack underflow");
+        return;
     }
+    s[top-i+1] = x;
 }
-
-void display() {
+void display(){
     int i;
-    if (top == -1) {
-        printf("Stack is empty.\n");
-    } else {
-        printf("Stack elements are:\n");
-        for (i = top; i >= 0; i--) {
-            printf("%d\n", stack[i]);
-        }
+    if (top < 0) {
+        printf("stack underflow");
+        return;
+    }
+    for (i = top; i >= 0; i--) {
+        printf("%d", s[i]);
     }
 }
 
-int main() {
-    int choice;
+void main(){
+    push(10);
+    push(20);
+    push(30);
 
-    do {
-        printf("\n--- STACK MENU ---\n");
-        printf("1. PUSH\n");
-        printf("2. POP\n");
-        printf("3. PEEP\n");
-        printf("4. CHANGE\n");
-        printf("5. DISPLAY\n");
-        printf("6. EXIT\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    printf("pop: %d\n", pop());
+    printf("peep: %d\n", peep(1));
 
-        switch (choice) {
-            case 1: push(); break;
-            case 2: pop(); break;
-            case 3: peep(); break;
-            case 4: change(); break;
-            case 5: display(); break;
-            case 6: printf("Exiting program.\n"); break;
-            default: printf("Invalid choice!\n");
-        }
-    } while (choice != 6);
-
-    return 0;
+    change(1, 100);
+    printf("\n");
+    display();
 }
